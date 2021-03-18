643ed40308f49fd133e8a8e06317e5d4bcf3747c
Edinelson Lima <edinelsonlima86@gmail.com>
2021-03-18 12:31:55 -0300

    Teste no VS CODE

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-//Carregando módulos
+//Carregando mï¿½dulos
     const express = require('express')
     const handlebars = require('express-handlebars')
     const bodyParser = require("body-parser")
     const app = express()
 
-
 //Modulos da Pasta Routes
     const admin = require("./routes/admin")
     const user = require("./routes/user")
     const recebeLogo = require("./routes/recebeLogo")
-    const recebeSlide1 = require("./routes/recebeSlide1")
+    const recebeSlide1= require("./routes/recebeSlide1")
     const recebeSlide2 = require("./routes/recebeSlide2")
     const recebeSlide3 = require("./routes/recebeSlide3")
 
